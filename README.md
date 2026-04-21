1960ef03c65a7fd446f540d42fc29bb291d9b961
segcore <44293604+segcore@users.noreply.github.com>
2026-04-21 23:47:35 +0800

    Add tree-sitter-jai support

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Neovim Configuration for programming in Jai
 
-A minimal neovim setup for programming in Jai
+A minimal neovim setup for programming in Jai. Probably requires neovim 0.12+.
+
+It configures:
+* [Jails](https://github.com/SogoCZE/Jails.git) LSP Server
+* [Treesitter-Jai](https://github.com/constantitus/tree-sitter-jai) for
+  highlighting and other treesitter features.
+* Keymap to open the Jai modules directory
 
 ```sh
 # Unix-like
@@ -11,3 +17,22 @@ git clone https://github.com/segcore/nvim-config %userprofile%\AppData\Local\nvi
 
 Or to test it out, call the final directory in the path something else (e.g.
 nvim-jai, not nvim) then run with `NVIM_APPNAME=nvim-jai nvim`.
+
+*There are additional build instructions inside the `init.lua` according to what
+you want to enable. They must also be followed for everything to work.*
+
+## Check if things are working
+
+After the configuration is setup AND you have followed the instructions inside
+the init.lua, open a `jai` file.
+* Treesitter should be highlighting the text
+  * Check if it is running with `:InspectTree`. It should should the treesitter parse tree.
+* Jails (LSP) should be setup and working
+  * Check health with `:checkhealth lsp`
+    ```
+    vim.lsp: Active Clients
+    - jails (id: 1)
+    ```
+  * Go-to definition with `ctrl-]` (get back with `ctrl-t`)
+  * More default key-mappings can be read at `:help lsp-defaults`
+    * In insert-mode, `ctrl-X ctrl-O` triggers the completion menu
--- init.lua
+++ init.lua
@@ -1,15 +1,60 @@
--- Recognise jai file type
-vim.filetype.add({ extension = { jai = 'jai' }})
+---- Recognise jai file type
+  vim.filetype.add({ extension = { jai = 'jai' }})
 
+---- Jails: the LSP server for Jai
+  -- https://github.com/SogoCZE/Jails.git
+  -- Install to path, or put the full path in cmd below
+  vim.lsp.config.jails = {
+    cmd = { 'jails' },
+    root_markers = { '.git', 'build.jai' },
+    filetypes = { 'jai' }
+  }
+  vim.lsp.enable('jails')
+  -- Check if things are working with `:checkhealth lsp`
+  -- Restart with `:lsp restart` (nvim 0.12+)
+
+---- Tree-sitter highlighting
+  -- Register {language} with {filetype}
+  vim.treesitter.language.register('jai', 'jai')
+
+  -- Build https://github.com/constantitus/tree-sitter-jai
+  --[[
+      git clone https://github.com/constantitus/tree-sitter-jai ~/opt/tree-sitter-jai
+      cd ~/opt/tree-sitter-jai
+      cmake . -B build -DCMAKE_BUILD_TYPE=Release
+      cmake --build build
+      # DLL now inside build/
+
+      # You must also copy the queries/ directory to your configuration path
+      # for highlighting etc. to work
+      cp ~/opt/tree-sitter-jai/queries/*  ~/.config/nvim-jai/queries/jai/
+    ]]
+  -- Tell neovim about the treesitter plugin and its DLL path
+  vim.treesitter.language.add('jai', { path = vim.fn.expand('~/opt/tree-sitter-jai/build/libtree-sitter-jai.so') })
+
+  -- Automatically start treesitter on filetypes that we have a treesitter installation for
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = '*',
+    group = vim.api.nvim_create_augroup('config-autotreesitter', { clear = true }),
+    callback = function(ev)
+      local filetype = vim.bo[ev.buf].filetype
+      local tsname = vim.treesitter.language.get_lang(filetype)
+      if tsname and vim.treesitter.language.add(tsname) then
+        vim.treesitter.start(ev.buf)
+        -- If treesitter AND vim regex highlighting modes are desired, turn it on like this
+        -- vim.bo[ev.buf].syntax = 'ON'
+      end
+    end,
+  })
+
+---- Other miscellaneous things
 -- Error format for compiler message recognition
-local jai = [[%f:%l\,%v: %t%\a\*:%m]]
-vim.o.errorformat = jai .. ',' .. vim.o.errorformat
+local jai_errfmt = [[%f:%l\,%v: %t%\a\*:%m]]
+vim.o.errorformat = jai_errfmt .. ',' .. vim.o.errorformat
 
--- Install to path: https://github.com/SogoCZE/Jails.git
-vim.lsp.config.jails = {
-  cmd = { 'jails', },
-  root_markers = { '.git', 'build.jai' },
-  filetypes = { 'jai' }
-}
-
-vim.lsp.enable({ 'jails' })
+-- keymap to open the Jai installation directory in a new tab, for :grep
+vim.keymap.set('n', '<space>sj', function()
+  local jaipath = '~/opt/jai'
+  vim.cmd.tabedit(jaipath .. '/modules/Basic/Print.jai')
+  vim.cmd.tcd(jaipath)
+end, { desc = 'Search Jai installation' })
